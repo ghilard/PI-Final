8fc45e93cff75bc7bae11040687320e561c865ab
Arthur Bello Ghilardi <arthurbeghilardi@gmail.com>
2024-11-28 12:55:11 -0300

    Correção do menu. Validar campos na criação do Quizz

--- js/script.js
+++ js/script.js
@@ -1,6 +1,56 @@
+//Permitir apenas com campo preenchidos
+let quest = document.getElementById("questao"); //Número da questão
+let pergunta = document.getElementById("apagar-pergunta");//Pergunta
+let titulo = document.getElementById("apagar-titulo");//Titulo
+let elemento = document.getElementsByClassName("apagar-alternativa");//Alternativas para resposta
+let select = document.getElementById('apagar-cronometro');//Limpar cronometro
+let checkboxes = document.querySelectorAll('.apagar-check');//Limpar checkbox
+
+function condicao(){
+    //Não permite campos vazios
+    if(pergunta.value == "" || titulo.value == "" || select.selectedIndex == 0)
+        {
+            alert("Pergunta, titulo, cronometro");
+            throw new Error("");
+        }
+        
+        //Qualquer alternativa sem valor PARA a execução
+        for (var i = 0; i < elemento.length; i++) {
+            if(elemento[i].value == ""){
+                alert("alternativas resposta");
+                throw new Error("");
+            }
+        }
+        
+        //Conferir checkBox selecionados, permitir apenas com um selecionado
+        let conferir = 0;
+        checkboxes.forEach(function(checkbox){
+            if(checkbox.checked == false){
+                conferir += 1;
+            }
+        
+            if(conferir == 4)
+            {
+                alert("Checkbox");
+                throw new Error("");
+            }
+        });
+}
+
+//Adicionar o link para navegar
+function linkNavegar(){
+    let link = document.getElementById('meuLink');
+
+    // Define o atributo href
+    link.href = '/paginas-adm/perfil-relatorio.html';
+}
+
 //Notificação de criação de quizz
 function ativarNotificacao() {
+    condicao();
+
     // Define a variável no localStorage
+    linkNavegar();
     localStorage.setItem('ativar', 'true');
 }
 
@@ -21,4 +71,44 @@ document.addEventListener('DOMContentLoaded', function () {
         localStorage.setItem('ativar', 'false');
     }
 });
+
+let NumberQuest = 1;
+
+function adicionar(){
+    condicao();
+    //Alterar numero da questão ----------------------
+    NumberQuest += 1;
+    quest.innerHTML = `Questão ${NumberQuest}`;
+    //-----------------------------------------------
+
+    //Limpar pergunta -----------------------------
+    pergunta.value = "";
+    //-----------------------------------------------
+
+    //Limpar titulo ---------------------------------
+    titulo.value = "";
+    //-----------------------------------------------
+
+    //Limpar alternativas ----------------------------
+    for (var i = 0; i < elemento.length; i++) {
+        elemento[i].value = ""; 
+    }
+    //------------------------------------------------
+
+    //Alterar cronometro ----------------------------
+    select.selectedIndex = 0;
+    //-----------------------------------------------
+
+    //Alterar checkBox -----------------------------
+    checkboxes.forEach(checkbox => {
+        checkbox.checked = false;
+    });
+    //------------------------------------------------
+
+}   
+
+
+
+
+
 
